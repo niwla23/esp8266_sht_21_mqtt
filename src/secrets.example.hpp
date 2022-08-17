6d5ed47aa5bfa46d9537207a737602fa686f4391
niwla23 <46248939+niwla23@users.noreply.github.com>
2022-08-17 17:41:58 +0200

    adjust example file

--- src/secrets.example.hpp
+++ src/secrets.example.hpp
@@ -4,3 +4,4 @@ const char* mqtt_server = "192.168.178.33";
 const char* temperatureTopic = "weather/terasse/temperature";
 const char* humidityTopic = "weather/terasse/humidity";
 const char* stateTopic = "weather/terasse/state";
+const char* rssiTopic = "weather/terasse/rssi";
